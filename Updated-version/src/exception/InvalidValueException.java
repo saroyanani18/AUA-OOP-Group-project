2bf77153520a244dde28863fac471a7bc78ee5a0
Annie <99117936+saroyanani18@users.noreply.github.com>
2022-05-12 22:18:13 +0400

    Delete Updated-version directory

--- Updated-version/src/exception/InvalidValueException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class InvalidValueException extends RuntimeException{
-    public InvalidValueException(String message){
-        super(message);
-    }
-}
--- Updated-version/src/exception/NotEnoughCashException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class NotEnoughCashException extends RuntimeException{
-    public NotEnoughCashException(String message) {
-        super(message);
-    }
-}
